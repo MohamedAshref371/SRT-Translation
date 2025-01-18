ffbe77bfc34166b631bdc1916280b2158a07a8de
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-01-18 15:43:49 +0200

    HttpClient global variable

--- SRT Translation/Translator.cs
+++ SRT Translation/Translator.cs
@@ -6,6 +6,8 @@ namespace SRT_Translation
 {
     public static class Translator
     {
+        private static readonly HttpClient client = new HttpClient();
+
         // https://stackoverflow.com/questions/43155233
         public static async Task<string> TranslateAsync(string text, string sourceLang, string targetLang)
         {
@@ -13,26 +15,23 @@ namespace SRT_Translation
 
             string url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={sourceLang}&tl={targetLang}&dt=t&q={escapedText}";
 
-            using (HttpClient client = new HttpClient())
+            try
             {
-                try
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+
+                int firstQuoteIndex = result.IndexOf("\"");
+                int secondQuoteIndex = result.IndexOf("\"", firstQuoteIndex + 1);
+                if (firstQuoteIndex != -1 && secondQuoteIndex != -1)
                 {
-                    HttpResponseMessage response = await client.GetAsync(url);
-                    response.EnsureSuccessStatusCode();
-
-                    string result = await response.Content.ReadAsStringAsync();
-
-                    int firstQuoteIndex = result.IndexOf("\"");
-                    int secondQuoteIndex = result.IndexOf("\"", firstQuoteIndex + 1);
-                    if (firstQuoteIndex != -1 && secondQuoteIndex != -1)
-                    {
-                        string translatedText = result.Substring(firstQuoteIndex + 1, secondQuoteIndex - firstQuoteIndex - 1);
-                        return translatedText;
-                    }
+                    string translatedText = result.Substring(firstQuoteIndex + 1, secondQuoteIndex - firstQuoteIndex - 1);
+                    return translatedText;
                 }
-                catch (Exception) { }
-                return "";
             }
+            catch (Exception) { }
+            return "";
         }
     }
 }
